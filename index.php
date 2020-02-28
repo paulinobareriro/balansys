2bf48a0b9e7e4bca79088abdd85c32d426a6b0e7
José paulino do Barreiro Silva <paulino.barreiro@balansys.pt>
2020-02-28 10:36:58 +0000

    version 280220201036

--- index.php
+++ index.php
@@ -77,7 +77,7 @@
     </div>
     
     <div class="hero-slide owl-carousel site-blocks-cover">
-      <div class="intro-section" style="background-image: url('images/hero_1.jpg');">
+      <div class="intro-section" style="background-image: url('images/banner.jpg');">
         <div class="container">
           <div class="row align-items-center">
             <div class="col-lg-12 mx-auto text-center" data-aos="fade-up">
