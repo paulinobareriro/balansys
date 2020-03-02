fcaab0aa02a93ef641fefbdb21887351cd3888e0
José paulino do Barreiro Silva <paulino.barreiro@balansys.pt>
2020-03-02 15:30:17 +0000

    version 02032020_1530

--- index.php
+++ index.php
@@ -274,7 +274,56 @@ if(isset($_POST['submit'])){
     
 
     <div class="footer">
-      <?php include 'includes/footer.php'; ?>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-3">
+            <p class="mb-4"><img src="images/logo_balancys.png" alt="Image" class="img-fluid"></p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae nemo minima qui dolor, iusto iure.</p>  
+            <p><a href="#">Learn More</a></p>
+          </div>
+          <div class="col-lg-3">
+            <h3 class="footer-heading"><span>Our Company</span></h3>
+            <ul class="list-unstyled">
+                <li><a href="#">Sobre Nós</a></li>
+                <!--<li><a href="#">News</a></li> -->
+                <li><a href="#">Nossos Serviços</a></li>
+                <li><a href="#"> Nossa Equipe Profissional</a></li>
+                <li><a href="#">Careers</a></li>
+                <li><a href="#">Projects</a></li>
+            </ul>
+          </div>
+          <div class="col-lg-3">
+              <h3 class="footer-heading"><span>Nossos Serviços</span></h3>
+              <ul class="list-unstyled">
+                  <li><a href="#">Industrial</a></li>
+                  <li><a href="#">Construction</a></li>
+                  <li><a href="#">Remodeling</a></li>
+              </ul>
+          </div>
+          <div class="col-lg-3">
+              <h3 class="footer-heading"><span>Contact</span></h3>
+              <ul class="list-unstyled">
+                  <li><a href="#">Help Center</a></li>
+                  <li><a href="#">Support Community</a></li>
+                  <li><a href="#">Press</a></li>
+                  <li><a href="#">FAQ</a></li>
+                  <li><a href="#">Our Partners</a></li>
+              </ul>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-12">
+            <div class="copyright">
+                <p>
+                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart text-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" >Colorlib</a>
+                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
     
 
