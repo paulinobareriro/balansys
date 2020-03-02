e0fa13450854c09f38072a2267ba637d76688d24
José paulino do Barreiro Silva <paulino.barreiro@balansys.pt>
2020-03-02 08:30:41 +0000

    version 02032020_0830

--- about.php
+++ about.php
@@ -195,7 +195,7 @@
               </div>
               <div class="service-1-content">
                 <h3 class="service-heading"> Nossa Equipe Profissional</h3>
-                <p>Profissionais, Qualificados, Dinâmicos E Apaixonados Pelo Que Fazem.</p>
+                <p>Profissionais, qualificados, dinâmicos e apaixonados pelo que fazem.</p>
               </div>
             </div>
           </div>
