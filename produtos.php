9edfdff5477e6d24b960a428392778bb40c860c5
José paulino do Barreiro Silva <paulino.barreiro@balansys.pt>
2020-03-09 08:17:45 +0000

    versión 09032020

--- produtos.php
+++ produtos.php
@@ -102,7 +102,7 @@
                     <div class="project-item">
                         <div class="project-item-contents">
                             <a href="project-single.html">
-                            <span class="project-item-category"></span>
+                            <span class="project-item-category">Descrição do produto</span>
                             <h2 class="project-item-title">
                                Células de carga digitais.
                             </h2>
