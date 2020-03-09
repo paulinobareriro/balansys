e02bfa0c848135b6c5a6e8547ab261cb649c1b9f
José paulino do Barreiro Silva <paulino.barreiro@balansys.pt>
2020-03-09 11:42:12 +0000

    versión 09032020_1142

--- produtos.php
+++ produtos.php
@@ -80,7 +80,7 @@
 
 
 
-    <div class="intro-section site-blocks-cover innerpage" style="background-image: url('images/celdas-md.jpg');">
+    <div class="intro-section site-blocks-cover innerpage" style="background-image: url('images/hero_1.jpg');">
       <div class="container">
         <div class="row align-items-center text-center border">
           <div class="col-lg-12 mt-5" data-aos="fade-up">
