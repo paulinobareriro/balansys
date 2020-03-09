c379be73797f48281219fad5191448690b8adf7e
José paulino do Barreiro Silva <paulino.barreiro@balansys.pt>
2020-03-09 09:26:50 +0000

    versión 09032020_026

--- load-cell.php
+++ load-cell.php
@@ -93,7 +93,7 @@
             <p class="text-white text-center">
               <a href="index.html">Home</a>
               <span class="mx-2">/</span>
-              <span>Project Single</span>
+              <span>Descrição do produto</span>
             </p>
           </div>
         </div>
@@ -108,8 +108,8 @@
                     <img src="images/hero_1.jpg" alt="Image" class="img-fluid">
                 </div>
                 <div class="col-md-4 ml-auto">
-                    <h2 class="text-black mb-4">Project Details Here</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum beatae maxime iure dolores labore? Corporis harum ea fuga voluptatum nulla! Consectetur, sint odit modi maxime harum enim dolores aliquid distinctio ad itaque commodi?</p>
+                    <h2 class="text-black mb-4">Vantagens e benefícios</h2>
+                    <p>Elimina os custos de manutenção e a incerteza de saber como a balança de seu caminhão está pesando.</p>
                     <p class="mb-5">Atque rerum fugiat iste voluptas, magni mollitia, cupiditate corporis perferendis veniam</p>
                     <p><a href="#" class="btn btn-primary rounded-0 py-3 px-5">Visit Site</a></p>
                 </div>
