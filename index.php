04b144f6cc58c41c67069a0500cc322033c55cea
José paulino do Barreiro Silva <paulino.barreiro@balansys.pt>
2020-03-10 16:33:57 +0000

    version PHPMailer 10032020_1633

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php 
 if(isset($_POST['submit'])){
 
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\Exception;
-
+   
 
     require 'PHPMailer/src/Exception.php';
     require 'PHPMailer/src/PHPMailer.php';
