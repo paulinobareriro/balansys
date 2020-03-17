6f0c314c73690d20211078079998888c6248ccc8
José paulino do Barreiro Silva <paulino.barreiro@balansys.pt>
2020-03-17 10:59:02 +0000

    version 17032020_1059

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ class VerificationCode
          *  YOUR PASSWORD 
          *  ************
          */               
-        $this->password = "*5689Balsys";  
+        $this->password = "hpux1020@";  
 
         /**
          * Receiver email
