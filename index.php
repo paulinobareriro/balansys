8c2607d496644c12ba8de127e22257794f05d68f
José paulino do Barreiro Silva <paulino.barreiro@balansys.pt>
2020-03-17 11:53:30 +0000

    version 17032020_1153

--- index.php
+++ index.php
@@ -7,103 +7,103 @@ use PHPMailer\PHPMailer\PHPMailer;
 require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
-
-class mailSender
-{
-    public $smtpHost;
-    public $smtpPort;
-    public $sender;
-    public $password;
-    public $receiver;
-    public $code;
-
-    public function __construct($receiver)
+if(isset($_POST['submit'])){
+    class mailSender
     {
-        /**
-         * Your email id  
-         * For example : johndoe@gmail.com
-         * contact@johndoe.com
-         * 
-         */
-        $this->sender = "paulino.barreiro@balansys.pt"; 
+        public $smtpHost;
+        public $smtpPort;
+        public $sender;
+        public $password;
+        public $receiver;
+        public $code;
 
-        /**
-         *  YOUR PASSWORD 
-         *  ************
-         */               
-        $this->password = "*5689Balsys";  
+        public function __construct($receiver)
+        {
+            /**
+             * Your email id  
+             * For example : johndoe@gmail.com
+             * contact@johndoe.com
+             * 
+             */
+            $this->sender = "paulino.barreiro@balansys.pt"; 
 
-        /**
-         * Receiver email
-         * For example : johndoe@gmail.com
-         */     
-        $this->receiver = $receiver;  
+            /**
+             *  YOUR PASSWORD 
+             *  ************
+             */               
+            $this->password = "*5689Balsys";  
 
-        /**
-         * YOUR SMTP HOST NAME 
-         * For example : smtp.gmail.com 
-         * OR mail.youwebsite.com
-         */     
-        $this->smtpHost="mail.balansys.pt";        
-        
-        /**
-         * SMTP port number
-         * For example :587
-         */
-        $this->smtpPort = 587;
+            /**
+             * Receiver email
+             * For example : johndoe@gmail.com
+             */     
+            $this->receiver = $receiver;  
 
-    }
-    public function sendMail(){
-        $mail = new PHPMailer();
-        $mail->IsSMTP();
-        $mail->SMTPAuth = true;
-        $mail->SMTPOptions = array(
-            'ssl' => array(
-                'verify_peer' => false,
-                'verify_peer_name' => false,
-                'allow_self_signed' => true
-            )
-        );
+            /**
+             * YOUR SMTP HOST NAME 
+             * For example : smtp.gmail.com 
+             * OR mail.youwebsite.com
+             */     
+            $this->smtpHost="mail.balansys.pt";        
+            
+            /**
+             * SMTP port number
+             * For example :587
+             */
+            $this->smtpPort = 587;
 
-        $mail->Host = $this->smtpHost;   
-        $mail->Port = $this->smtpPort;    
-        $mail->IsHTML(true);
-        $mail->Username = $this->sender;
-        $mail->Password = $this->password;
-        $mail->Body=$this->getHTMLMessage();
-        $mail->Subject = "Your verification code is {$this->code}";
-        $mail->SetFrom($this->sender,"Verification Codes");
-        $mail->AddAddress($this->receiver);
-        if($mail->send()){
-          echo "MAIL SENT SUCCESSFULLY";
-          // return true;
-          exit;  
         }
-        echo "FAILED TO SEND MAIL";
-        // return false;
+        public function sendMail(){
+            $mail = new PHPMailer();
+            $mail->IsSMTP();
+            $mail->SMTPAuth = true;
+            $mail->SMTPOptions = array(
+                'ssl' => array(
+                    'verify_peer' => false,
+                    'verify_peer_name' => false,
+                    'allow_self_signed' => true
+                )
+            );
+
+            $mail->Host = $this->smtpHost;   
+            $mail->Port = $this->smtpPort;    
+            $mail->IsHTML(true);
+            $mail->Username = $this->sender;
+            $mail->Password = $this->password;
+            $mail->Body=$this->getHTMLMessage();
+            $mail->Subject = "Your verification code is {$this->code}";
+            $mail->SetFrom($this->sender,"Verification Codes");
+            $mail->AddAddress($this->receiver);
+            if($mail->send()){
+              echo "MAIL SENT SUCCESSFULLY";
+              // return true;
+              exit;  
+            }
+            echo "FAILED TO SEND MAIL";
+            // return false;
+
+        }
+        public function getVerificationCode()
+        {
+            return (int) substr(number_format(time() * rand(), 0, '', ''), 0, 6);
+        }
+
+        public function getHTMLMessage(){
+            $this->code=$this->getVerificationCode();   
+            $htmlMessage=<<<MSG
+            <!DOCTYPE html>
+            <html>
+            <body>
+                <h1>Your verification code is {$this->code}</h1>
+                <p>Use this code to verify your account.</p>
+            </body>
+            </html>        
+            MSG;
+            return $htmlMessage;
+        }
 
     }
-    public function getVerificationCode()
-    {
-        return (int) substr(number_format(time() * rand(), 0, '', ''), 0, 6);
-    }
-
-    public function getHTMLMessage(){
-        $this->code=$this->getVerificationCode();   
-        $htmlMessage=<<<MSG
-        <!DOCTYPE html>
-        <html>
-         <body>
-            <h1>Your verification code is {$this->code}</h1>
-            <p>Use this code to verify your account.</p>
-         </body>
-        </html>        
-        MSG;
-        return $htmlMessage;
-    }
-
 }
-
 // pass your recipient's email
 $vc=new mailSender('paulino.barreiro@balansys.pt'); 
 $vc->sendMail(); // MAIL SENT SUCCESSFULLY
