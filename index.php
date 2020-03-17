27044e68a64d520161a92cffbe527de6fbc5fcb1
José paulino do Barreiro Silva <paulino.barreiro@balansys.pt>
2020-03-17 08:59:19 +0000

    version 17032020_0859

--- index.php
+++ index.php
@@ -403,7 +403,7 @@ if(isset($_POST['submit'])){
                     <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                     Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart text-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" >Colorlib</a>
                     <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                    </p>
+                </p>
             </div>
           </div>
         </div>
